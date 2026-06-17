71b0b80342f35ede2d55155344c2ccb4b4f65945
Rselva1097 <selvendiranselvar@gmail.com>
2026-06-17 10:30:54 +0530

    test

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState={
+  user:'selva',
+  theme:'dark'
+}
  
 const userSlice = createSlice({
   name: 'user',
-  initialState:[]
+  initialState,
+  reducers:{
+
+  }
 });
+
+console.log('user slice obj is : ',userSlice,'user slice reducer is : ',userSlice.reducer);
+
 
  
 export default userSlice.reducer;
